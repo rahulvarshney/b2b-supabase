1eb578d1513006c7379c9f3a700e4ff69ee6d1a0
R V <2096tester@gmail.com>
2022-08-26 16:16:50 -0700

    using dotenv -  Deno.env.get, adding .gitignore

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,9 +2,14 @@ import "dotenv";
 import { supabaseClient, supabaseTable, supabaseTableItems } from "supabase";
 
 const sbclient = new supabaseClient(
+    Deno.env.get("SUPABASE_API_URL"),
+    Deno.env.get("SUPABASE_ANON_KEY")   
+);
+
+/*const sbclient = new supabaseClient(
     "https://xlrlffigpvvuhrfvqoxm.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhscmxmZmlncHZ2dWhyZnZxb3htIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAzNDUzMTQsImV4cCI6MTk3NTkyMTMxNH0.A1mV9suLGPExpkWka_S-EA9ZccNszqjmZWeLIbWBsvE"
-);
+);*/
 
 const sbTableItems = new supabaseTableItems(sbclient, "logos");
 
