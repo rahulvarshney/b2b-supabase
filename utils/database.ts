c15cf8c2d24e3dd0f3496bffe7104e9bd24fe72e
R V <2096tester@gmail.com>
2022-08-26 16:35:17 -0700

    renamed supabase.ts to database.ts

--- utils/database.ts
+++ utils/database.ts
@@ -6,11 +6,6 @@ const sbclient = new supabaseClient(
     Deno.env.get("SUPABASE_ANON_KEY")   
 );
 
-/*const sbclient = new supabaseClient(
-    "https://xlrlffigpvvuhrfvqoxm.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhscmxmZmlncHZ2dWhyZnZxb3htIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAzNDUzMTQsImV4cCI6MTk3NTkyMTMxNH0.A1mV9suLGPExpkWka_S-EA9ZccNszqjmZWeLIbWBsvE"
-);*/
-
 const sbTableItems = new supabaseTableItems(sbclient, "logos");
 
 console.log(await sbTableItems.all());
