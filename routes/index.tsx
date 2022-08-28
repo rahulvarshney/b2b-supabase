250a3bcc31f7ff53f34b4daf1103413bc3a84c23
R V <2096tester@gmail.com>
2022-08-28 07:28:27 -0700

    database.ts in api folder

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,9 +3,28 @@ import { h } from "preact";
 import { tw } from "@twind";
 import Counter from "../islands/Counter.tsx";
 import type { LogoView } from "@/utils/types.ts";
+//import * as database from "@/utils/types.ts";
+import "dotenv";
+import { supabaseClient, supabaseTable, supabaseTableItems } from "supabase";
 
-export default function Home() {
-  
+
+
+/*const sbclient = new supabaseClient(
+    Deno.env.get("SUPABASE_API_URL"),
+    Deno.env.get("SUPABASE_ANON_KEY")   
+);
+
+const sbTableItems = new supabaseTableItems(sbclient, "logos");
+
+const sbItemTest = await sbTableItems.get('b2b_domain', '740esquire.com');
+
+console.log( sbItemTest );*/
+
+
+
+
+export default function Home( ) {
+
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <img
@@ -15,7 +34,8 @@ export default function Home() {
       />
       <p class={tw`my-6`}>
         Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-        file, and refresh.
+        file, and refresh. 0-0-2
+
       </p>
       <Counter start={3} />
     </div>
